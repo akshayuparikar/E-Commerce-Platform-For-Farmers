d94a608a9e5630ed7f620730ba625a4425eb72ff
Akshay Uparikar <akshayuparikar2002@gmail.com>
2025-11-04 00:26:26 +0530

    Add files via upload

---
+++ template/nav-bar.php
@@ -0,0 +1,22 @@
+<!-- nav-bar.php -->
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+  	<div class="container">
+	    <a class="navbar-brand" href="index.php" style="border: none"><img src="images/agrolink.png" alt="agrolink" width="150"></a>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="oi oi-menu"></span> Menu
+	    </button>
+
+	    <div class="collapse navbar-collapse" id="ftco-nav">
+	      <ul class="navbar-nav ml-auto">
+	        <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+	      
+	         <!-- <li class="nav-item"><a href="buyer/buyer/register-buyer.php" class="nav-link">Buyer Registration</a></li> -->
+	       	 <li class="nav-item"><a href="buyer/buyer/index.php" class="nav-link">Buyer Login</a></li>
+				<li class="nav-item"><a href="farmer/farmer/index.php" class="nav-link">Farmer Login</a></li>
+	       	
+	        <li class="nav-item"><a href="admin/admin/index.php" class="nav-link">Admin Login</a></li>
+	      </ul>
+	    </div>
+  	</div>
+</nav>
